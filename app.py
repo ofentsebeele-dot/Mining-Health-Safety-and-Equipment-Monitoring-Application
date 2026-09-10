35efb0f2247d0fa66dd8329c804f8fa11af6972e
ofentsebeele-dot <ofentsebeele@gmail.com>
2026-09-10 15:32:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.tittle("Mining Health, Safety and Equipment Monitoring Application")
+st.title("Mining Health, Safety and Equipment Monitoring Application")
 st.write("Welcome to the Application")
 st.success("Stramlit is working")
