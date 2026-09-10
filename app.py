c90700529a69a82303300e3a51037ee09d3f5158
ofentsebeele-dot <ofentsebeele@gmail.com>
2026-09-10 18:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 st.title("Mining Health, Safety and Equipment Monitoring Application")
 st.write("Welcome to the Application")
 st.success("Streamlit is working")
+#Login portal
+users = { "admin": {"password": "admin123", "role": "Administrator"},
+         "safety": {"password": "safety123", "role": "Safety Officer"},
+         "mining": {"password": "mining123", "role": "Mining Engineer"},
+         "maintenance": {"password": "maintanace123", "role": "Maintenance Engineer"},
+         "manager": {"password": "manager123", "role": "Manager" }}
