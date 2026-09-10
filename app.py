2bf765a88ee0e95955eeb9e881a3f4b59cce0a04
ofentsebeele-dot <ofentsebeele@gmail.com>
2026-09-10 19:04:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-#Login portal
 import streamlit as st
 users = { "admin": {"password": "admin123", "role": "Administrator"},
          "safety": {"password": "safety123", "role": "Safety Officer"},
          "mining": {"password": "mining123", "role": "Mining Engineer"},
          "maintenance": {"password": "maintanace123", "role": "Maintenance Engineer"},
          "manager": {"password": "manager123", "role": "Manager" }}
-username = input("enter your username:")
-password = input("enter your password:")
-if username in users:
+st.title("Mining Health, Safety and Equipment Monitoring Application")
+st.subheader("Login")
+username = st.text_input("enter your username:")
+password = st.text_input("enter your password:")
+if st.username in users:
     if password == users[username]["password"]:
         role = users[username]["role"]
         print("Login Successful")
         print("Role",role)
     else:
-        print("Incorrect Password")
+        st.error("Incorrect Password")
 else:
-    print("Username not found")
+    st.error("Username not found")
