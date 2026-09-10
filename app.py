de888ecd3eb155d634ab55eae1ff7c7e41d287bd
ofentsebeele-dot <ofentsebeele@gmail.com>
2026-09-10 18:53:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ users = { "admin": {"password": "admin123", "role": "Administrator"},
          "mining": {"password": "mining123", "role": "Mining Engineer"},
          "maintenance": {"password": "maintanace123", "role": "Maintenance Engineer"},
          "manager": {"password": "manager123", "role": "Manager" }}
+username = input("enter your username:")
+password = input("enter your password:")
+if username in users:
+    if password == users[username]["password"]:
+        role = users[username]["role"]
+        print("Login Successful")
+        print("Role",role)
+    else:
+        print("Incorrect Password")
+else:
+    print("Username not found")
